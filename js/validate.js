f5ebefb8453d2b8a3a490ef23776c6152ea95d25
rohansood98 <rohansood98@gmail.com>
2020-08-29 13:32:23 +0530

    JS validation

---
+++ js/validate.js
@@ -0,0 +1,47 @@
+function echeck(){
+console.log("hello")
+ var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+ 		if(document.getElementById('email').value=="")
+ 		{
+ 			document.getElementById('email').innerHTML='This is a required field';
+ 		}
+
+        else if (reg.test(document.getElementById('email').value)==false) 
+        {
+            document.getElementById('email').innerHTML='This is not a valid email address';
+            
+        }
+
+        
+
+}
+
+function tcheck(){
+var reg=/^\+?(\d[\d\-\+\(\) ]{13}\d$)/;
+console.log(document.getElementById('tel').value.length)
+if(document.getElementById('tel').value=="")
+ 		{
+ 			document.getElementById('tel').innerHTML='This is a required field';
+ 		}
+if(String(document.getElementById('tel').value).length!==12 || reg.test(String(document.getElementById('tel').value))){
+document.getElementById('tel').innerHTML="This is not a valid phone number";
+}
+}
+function ncheck(){
+var reg=/^\+?(\d[\d\-\+\(\) ]{13}\d$)/;
+if(document.getElementById('nog').value=="")
+ 		{
+ 			document.getElementById('nog').innerHTML='This is a required field';
+ 		}
+if( reg.test(String(document.getElementById('nog').value))){
+document.getElementById('nog').innerHTML="This is not a valid guest number";
+}
+}
+function datecheck(){
+	if(document.getElementById('date').value=="")
+ 		{
+ 			document.getElementById('date').innerHTML='This is a required field';
+ 		}
+
+}
+
